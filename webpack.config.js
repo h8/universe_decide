21d74c84687757a8140be7c3eb50bd316779b78a
Dmitry Stropalov <d.stropalov@gmail.com>
2018-01-06 13:31:17 +0100

    Introduce copy-webpack plugin
    Added copy-webpack plugin for static assets like images and other files.
    Boilerplate robots.txt and favicon.ico were added also.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   entry: __dirname + "/assets/js/app.js",
   output: {
     path: __dirname + "/priv/static",
     filename: "js/app.js"
-  }
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([{from: __dirname + "/assets/static"}])
+  ]
 };
